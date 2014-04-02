151cd05ba239fb8e8abb7e6bcd0468c7065d9eb0
cressie176 <scresswell@acuminous.co.uk>
2014-04-02 18:58:20 +0100

    Still playing with install script

--- bin/install.bat
+++ bin/install.bat
@@ -1,19 +1,5 @@
 @echo off
 setlocal
-
-:check_bower
-where bower > nul 2>&1
-if not errorlevel 1 goto check_gulp
-echo Could not find bower
-exit /b 1
-
-:check_gulp
-where gulp > nul 2>&1
-if not errorlevel 1 goto install
-echo Could not find gulp
-exit /b 1
-
-:install
-bower install
-gulp build
+node_modules\.bin\bower install
+node_modules\.bin\gulp build
 endlocal
--- bin/install.sh
+++ bin/install.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-which bower > /dev/null
-if [ $? -ne 0 ]; then
-    echo "Could not find bower"
-    exit 1;
-fi
-
-which gulp > /dev/null
-if [ $? -ne 0 ]; then
-    echo "Could not find gulp"
-    exit 1;
-fi
-
-bower install
-gulp build
+node_modules/.bin/bower install
+node_modules/.bin/gulp build
